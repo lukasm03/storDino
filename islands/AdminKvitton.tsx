e52ea1041ad62244c5054bf58b6cf0e92b52854b
Lukas Mörtman <lukas@mocko.se>
2022-10-19 18:58:36 +0200

    testing stuff

--- islands/AdminKvitton.tsx
+++ islands/AdminKvitton.tsx
@@ -1,8 +1,6 @@
 import { useState } from "preact/hooks";
 import ettKvittoiListan from "../components/Kvitto.tsx";
 
-
-
 export default function VisaKvitton({ data }) {
   const [visa, setVisa] = useState("intefixade")
 
@@ -48,38 +46,8 @@ export default function VisaKvitton({ data }) {
           <h3 style={{ marginBottom: "0" }}>senaste kvitton:</h3>
           {data.slice(0).reverse().map(({ id, Vara, Pris, Kategori, Datum, Swish, Bild, Typavköp, Fixad }) => {
             if (Fixad === false) {
-              return <div style={{display:"grid",gridTemplateColumns:"repeat(1, 1fr)",gridTemplateRows:"repeat(4, 1fr)",gridColumnGap:"0px",gridRowGap:"0px",paddingBottom:"1rem",paddingTop:"0.5rem"}} key={Vara}>
-        <div style={{    gridArea: "1 / 1 / 6 / 2", 
-    alignSelf: "center", 
-    marginRight: "1rem", 
-    justifyContent: "center", height: "80px", width: "80px"}}>
-            <img src={Bild} alt={"bild på kvittot"} height={"80px"} width={"80px"}/>
-        </div>
-        <div style={{gridArea: "1 / 2 / 2 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>namn
-            på {Typavköp}: {Vara} </p>
-        </div>
-        <div style={{gridArea: "2 / 2 / 3 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>pris
-            på {Typavköp}: {Pris}kr</p>
-        </div>
-        <div style={{gridArea: "3 / 2 / 4 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>kategori
-            på {Typavköp}: {Kategori}</p>
-        </div>
-        <div style={{gridArea: "4 / 2 / 5 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>datum {Typavköp}en
-            skedde: {Datum}</p>
-        </div>
-        <div style={{ gridArea: "5 / 2 / 6 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>swishnummer: {Swish}</p>
-        </div>
-    </div>            }
+              return ettKvittoiListan(id, Vara, Pris, Kategori, Datum, Swish, Bild, Typavköp, Fixad)
+            }
           })}
         </div>
       </>
@@ -120,38 +88,8 @@ export default function VisaKvitton({ data }) {
           {data.slice(0).reverse().map(({ id, Vara, Pris, Kategori, Datum, Swish, Bild, Typavköp, Fixad }) => {
             if (Typavköp === "Intäkt") {
               return (
-                <div style={{display:"grid",gridTemplateColumns:"repeat(1, 1fr)",gridTemplateRows:"repeat(4, 1fr)",gridColumnGap:"0px",gridRowGap:"0px",paddingBottom:"1rem",paddingTop:"0.5rem"}} key={Vara}>
-        <div style={{    gridArea: "1 / 1 / 6 / 2", 
-    alignSelf: "center", 
-    marginRight: "1rem", 
-    justifyContent: "center", height: "80px", width: "80px"}}>
-            <img src={Bild} alt={"bild på kvittot"} height={"80px"} width={"80px"}/>
-        </div>
-        <div style={{gridArea: "1 / 2 / 2 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>namn
-            på {Typavköp}: {Vara} </p>
-        </div>
-        <div style={{gridArea: "2 / 2 / 3 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>pris
-            på {Typavköp}: {Pris}kr</p>
-        </div>
-        <div style={{gridArea: "3 / 2 / 4 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>kategori
-            på {Typavköp}: {Kategori}</p>
-        </div>
-        <div style={{gridArea: "4 / 2 / 5 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>datum {Typavköp}en
-            skedde: {Datum}</p>
-        </div>
-        <div style={{ gridArea: "5 / 2 / 6 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>swishnummer: {Swish}</p>
-        </div>
-    </div>              )
+                ettKvittoiListan(id, Vara, Pris, Kategori, Datum, Swish, Bild, Typavköp, Fixad)
+              )
             }
           })}
         </div>
@@ -193,38 +131,8 @@ export default function VisaKvitton({ data }) {
           {data.slice(0).reverse().map(({ id, Vara, Pris, Kategori, Datum, Swish, Bild, Typavköp, Fixad }) => {
             if (Typavköp === "Avgift") {
               return (
-                <div style={{display:"grid",gridTemplateColumns:"repeat(1, 1fr)",gridTemplateRows:"repeat(4, 1fr)",gridColumnGap:"0px",gridRowGap:"0px",paddingBottom:"1rem",paddingTop:"0.5rem"}} key={Vara}>
-        <div style={{    gridArea: "1 / 1 / 6 / 2", 
-    alignSelf: "center", 
-    marginRight: "1rem", 
-    justifyContent: "center", height: "80px", width: "80px"}}>
-            <img src={Bild} alt={"bild på kvittot"} height={"80px"} width={"80px"}/>
-        </div>
-        <div style={{gridArea: "1 / 2 / 2 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>namn
-            på {Typavköp}: {Vara} </p>
-        </div>
-        <div style={{gridArea: "2 / 2 / 3 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>pris
-            på {Typavköp}: {Pris}kr</p>
-        </div>
-        <div style={{gridArea: "3 / 2 / 4 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>kategori
-            på {Typavköp}: {Kategori}</p>
-        </div>
-        <div style={{gridArea: "4 / 2 / 5 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>datum {Typavköp}en
-            skedde: {Datum}</p>
-        </div>
-        <div style={{ gridArea: "5 / 2 / 6 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>swishnummer: {Swish}</p>
-        </div>
-    </div>              )
+                ettKvittoiListan(id, Vara, Pris, Kategori, Datum, Swish, Bild, Typavköp, Fixad)
+              )
             }
           })}
         </div>
@@ -265,38 +173,8 @@ export default function VisaKvitton({ data }) {
         }}>                    <h3 style={{ marginBottom: "0" }}>senaste kvitton:</h3>
           {data.slice(0).reverse().map(({ id, Vara, Pris, Kategori, Datum, Swish, Bild, Typavköp, Fixad }) => {
             return (
-              <div style={{display:"grid",gridTemplateColumns:"repeat(1, 1fr)",gridTemplateRows:"repeat(4, 1fr)",gridColumnGap:"0px",gridRowGap:"0px",paddingBottom:"1rem",paddingTop:"0.5rem"}} key={Vara}>
-        <div style={{    gridArea: "1 / 1 / 6 / 2", 
-    alignSelf: "center", 
-    marginRight: "1rem", 
-    justifyContent: "center", height: "80px", width: "80px"}}>
-            <img src={Bild} alt={"bild på kvittot"} height={"80px"} width={"80px"}/>
-        </div>
-        <div style={{gridArea: "1 / 2 / 2 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>namn
-            på {Typavköp}: {Vara} </p>
-        </div>
-        <div style={{gridArea: "2 / 2 / 3 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>pris
-            på {Typavköp}: {Pris}kr</p>
-        </div>
-        <div style={{gridArea: "3 / 2 / 4 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>kategori
-            på {Typavköp}: {Kategori}</p>
-        </div>
-        <div style={{gridArea: "4 / 2 / 5 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>datum {Typavköp}en
-            skedde: {Datum}</p>
-        </div>
-        <div style={{ gridArea: "5 / 2 / 6 / 3", 
-    alignSelf: "center", 
-    justifyContent: "center"}}><p style={{height: "fit-content", margin: 0}}>swishnummer: {Swish}</p>
-        </div>
-    </div>            )
+              ettKvittoiListan(id, Vara, Pris, Kategori, Datum, Swish, Bild, Typavköp, Fixad)
+            )
           })
           }
         </div>
